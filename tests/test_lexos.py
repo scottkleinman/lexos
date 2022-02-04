1ccef046a496fcfadb59b7fd33113cd611cd3d9e
Scott Kleinman <scott.kleinman@csun.edu>
2022-02-04 10:22:43 -0800

    Add tests and continuous integration

--- tests/test_lexos.py
+++ tests/test_lexos.py
@@ -3,3 +3,7 @@ from lexos import __version__
 
 def test_version():
     assert __version__ == '0.0.1'
+
+def test_initials():
+    from lexos.initials import initials
+    assert initials('Guide van Rossum') == 'GvR'
