81536e8830609cf71409d77445a5d1f381c0b2e0
Scott Kleinman <scott.kleinman@csun.edu>
2022-02-04 16:15:44 -0800

    Add Scrubber class (no tests)

---
+++ lexos/scrubber/scrubber.py
@@ -0,0 +1,67 @@
+"""scrubber.py.
+
+    This file contains the main logic for the Scrubber class.
+"""
+from inspect import getfullargspec
+from typing import Callable, List, Union
+from lexos import utils
+from . import pipeline
+from lexos.exceptions import LexosException
+
+class Scrubber:
+    """Scrubber class.
+
+    Sample usage:
+
+        scrubber = Scrubber()
+        scrubber.to_lower(doc)
+    """
+    def __init__(self):
+        self.texts = []
+        self.pipeline = None
+
+    def add_pipeline(self, *funcs: Callable[[str], str]):
+        """Add a pipeline.
+
+        Args:
+            *funcs: The functions to add to the pipeline.
+        """
+        self.pipeline = pipeline.make_pipeline(funcs)
+
+    def get_pipeline(self):
+        """Return a tuple representation of the pipeline."""
+        pipeline = []
+        for f in self.pipeline:
+            if getfullargspec(f).kwonlydefaults:
+                pipeline.append((f.__name__, getfullargspec(f).kwonlydefaults))
+            else:
+                pipeline.append(f.__name__)
+        return tuple(pipeline)
+
+    def set_pipeline(self, pipeline: tuple):
+        """Set the pipeline.
+
+        This is a variant of add_pipeline that takes a tuple of functions.
+        The difference is that function names are given as strings and
+        keyword arguments as a dictionary. This is useful if you wanted to
+        modify the pipeline after initialisation based on the output of
+        `get_pipeline()`, rather than passing callables.
+        """
+        new_pipeline = []
+        for x in pipeline:
+            if isinstance(x, tuple):
+                new_pipeline.append(new_pipeline.pipe(eval(x[0]), **x[1]))
+            else:
+                new_pipeline.append(eval(x))
+        self.pipeline = pipeline.make_pipeline(new_pipeline)
+
+    def scrub(self, data: Union[List[str], str]):
+        """Scrub a text or list of texts.
+
+        Args:
+            data (Union[List[str], str]): The text or list of texts to scrub.
+        """
+        for text in utils.ensure_list(data):
+            print(type(text))
+            self.texts.append(self.pipeline(text))
+
