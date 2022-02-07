9f929ee637e6b1fa9eff0a057bc3f18e7b019f36
Scott Kleinman <scott.kleinman@csun.edu>
2022-02-06 22:26:47 -0800

    Add get_term_counts method

--- lexos/tokenizer/lexosdoc.py
+++ lexos/tokenizer/lexosdoc.py
@@ -1,5 +1,8 @@
 """lexosdoc.py."""
-from typing import List
+from typing import Any, Dict, List, Union
+
+from collections import Counter
+import re
 
 import pandas as pd
 
@@ -19,6 +22,49 @@ class LexosDoc():
     """
     def __init__(self, doc: object):
         self.doc = doc
+
+    def get_term_counts(self,
+                        limit: int = None,
+                        start: Any = 0,
+                        end: Any = None,
+                        filters: List[Union[Dict[str, str], str]] = None,
+                        regex: bool = False,
+                        as_df = False) -> Union[List, pd.DataFrame]:
+        """Get a list of word counts for each token in the doc.
+
+        Args:
+            self: A spaCy doc.
+            limit: The maximum number of tokens to count.
+            start: The index of the first token to count.
+            end: The index of the last token to count after limit is applied.
+            filters: A list of Doc attributes to ignore.
+            regex (bool): Whether to match the dictionary value using regex.
+            as_df: Whether to return a pandas dataframe.
+
+        Returns:
+            Union[List, pd.DataFrame]: A list of word count tuples for
+            each token in the doc. Alternatively, a pandas dataframe.
+        """
+        tokens = []
+        bool_filters = []
+        dict_filters = {}
+        if filters:
+            for filter in filters:
+                if isinstance(filter, dict):
+                    dict_filters[list(filter.keys())[0]] = list(filter.values())[0]
+                else:
+                    bool_filters.append(filter)
+        tokens = [
+            token.text
+            for token in self.doc
+            if self._bool_filter(token, bool_filters)
+            and self._dict_filter(token, dict_filters, regex=regex)
+        ]
+        term_counts = Counter(tokens).most_common(limit)[start:end]
+        if as_df:
+            return pd.DataFrame(term_counts, columns=["term", "count"])
+        else:
+            return term_counts
 
     def get_tokens(self):
         """Return a list of tokens in the doc."""
@@ -66,3 +112,45 @@ class LexosDoc():
         if show_ranges:
             cols = cols + ["start", "end"]
         return pd.DataFrame(rows, columns=cols)
+
+    def _bool_filter(self, token: object, filters: List[str]) -> bool:
+        """Filter a token based on a list of boolean filters.
+
+        Args:
+            token (object): A spaCy token.
+            filters (str): A list of boolean filters (the names of spaCy token attributes).
+
+        Returns:
+            bool: Whether the token passes the filters.
+        """
+        if filters and filters != []:
+            for filter in filters:
+                if getattr(token, filter):
+                    return False
+                else:
+                    return True
+        else:
+            return True
+
+    def _dict_filter(self, token: object, filters: List[Dict[str, str]], regex: bool = False) -> bool:
+        """Filter a token based on a list of dictionary filters.
+
+        Args:
+            token (object): A spaCy token.
+            filters (List[Dict[str, str]]): A list of filter dictionaries with keys
+            as spaCy token attributes.
+            regex (bool): Whether to match the dictionary value using regex.
+
+        Returns:
+            bool: Whether the token passes the filters.
+        """
+        if filters and filters != {}:
+            for filter, value in filters.items():
+                if regex and re.search(re.compile(value), getattr(token, filter)) is not None:
+                    return False
+                elif getattr(token, filter) == value:
+                    return False
+                else:
+                    return True
+        else:
+            return True
