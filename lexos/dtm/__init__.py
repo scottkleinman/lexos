ff4393d979f632686e3fe03b271264d65532f751
Scott Kleinman <scott.kleinman@csun.edu>
2022-06-30 14:50:35 -0700

    Add validation and error handling

--- lexos/dtm/__init__.py
+++ lexos/dtm/__init__.py
@@ -1,34 +1,81 @@
 """dtm.py."""
 import re
-from collections import Counter
 from typing import Any, Dict, List, Union
 
 import pandas as pd
+import spacy
 from natsort import natsort_keygen, ns
 
-from lexos.tokenizer.lexosdoc import LexosDoc
+from pydantic import BaseModel, ValidationError, validator
+
+from lexos.exceptions import LexosException
 
 # This should probably be moved to constants.py
 SORTING_ALGORITHM = natsort_keygen(alg=ns.LOCALE)
 
 
+class DtmData(BaseModel):
+    """DtmData class.
+
+    This model validates the input data for the DTM and, if necessary,
+    coerces it to a list of token lists.
+    """
+
+    docs: List[Union[List[str], spacy.tokens.doc.Doc]]
+
+    class Config:
+        """Config class."""
+
+        arbitrary_types_allowed = True
+
+    @validator("docs", pre=True, always=True)
+    def ensure_token_lists(cls, v):
+        """Coerces input to a list of token lists where each token is a string."""
+        tokens = []
+        for doc in v:
+            if isinstance(doc, spacy.tokens.doc.Doc):
+                tokens.append([token.text for token in doc])
+            elif isinstance(doc, list):
+                if all(isinstance(sub, str) for sub in doc):
+                    tokens.append(doc)
+                else:
+                    raise LexosException("Each list item must be a string.")
+            else:
+                raise LexosException("Could not parse the document list.")
+        return tokens
+
+
 class DTM:
     """Class for a document-term matrix."""
 
-    def __init__(self, docs=List[Union[list, object]], labels=List[str]):
-        """Initialise the DTM."""
+    def __init__(
+        self,
+        docs=List[Union[List[str], spacy.tokens.doc.Doc]],
+        labels: List[str] = None,
+    ) -> None:
+        """Initialise the DTM.
+
+        Args:
+            docs (List[Union[List[str], spacy.tokens.doc.Doc]]): A list of spaCy docs or a list of token lists.
+            labels (List[str]): A list of labels for the documents.
+
+        Returns:
+            None
+        """
         self.docs = docs
         self.table = None
-        self.labels = labels
+        if not labels:
+            self.labels = ["doc" + str(i) for i in range(len(docs))]
+        else:
+            self.labels = labels
         self.vectorizer_settings = {}
         self.vectorizer = self.set_vectorizer(new=True)
         self.build()
 
     def build(self):
         """Build a new DTM matrix based on the current vectorizer."""
-        # doc_tokens = [[token.text for token in doc] for doc in self.docs]
-        doc_tokens = self._validate_input()
-        self.matrix = self.vectorizer.fit_transform(doc_tokens)
+        doc_token_lists = DtmData(docs=self.docs).docs
+        self.matrix = self.vectorizer.fit_transform(doc_token_lists)
         # Require explicit calling of get_table after each build to ensure table is up to date.
         # Ensures that the two processes can be kept separate if desired.
         self.table = None
@@ -97,11 +144,11 @@ class DTM:
         df = self.get_table()
         tmp = df.copy()
         if "sum" in stats:
-            tmp["sum"] = df.sum(axis=1)
+            tmp["sum"] = df.sum(axis=1, numeric_only=True)
         if "mean" in stats:
-            tmp["mean"] = df.mean(axis=1).round(rounding)
+            tmp["mean"] = df.mean(axis=1, numeric_only=True).round(rounding)
         if "median" in stats:
-            median = df.median(axis=1)
+            median = df.median(axis=1, numeric_only=True)
             tmp["median"] = median.round(rounding)
         return tmp
 
@@ -124,6 +171,8 @@ class DTM:
         Returns:
             List(tuple): A list of tuples containing terms and counts.
         """
+        if alg != SORTING_ALGORITHM:
+            self._validate_sorting_algorithm(alg)
         df = self.get_stats_table("sum").sort_values(
             by=sort_by, ascending=ascending, key=alg
         )
@@ -143,7 +192,6 @@ class DTM:
 
         Note: This function should not be used if `min_df` or `max_df` is set in
         the vectorizer because the table will be cut twice.
-
         """
         df = self.get_stats_table("sum").sort_values(by="sum", ascending=True)
         return df[start:max_n_terms]
@@ -205,132 +253,21 @@ class DTM:
         else:
             self.vectorizer = vectorizer
 
-    def _validate_input(self) -> List[List[str]]:
-        """Make sure that the DTM input consists of valid token lists or spaCy documents.
+    def _validate_sorting_algorithm(self, alg: Any) -> bool:
+        """Ensure that the specified sorting algorithm is a valid natsort locale.
+
+        Args:
+            alg: The sorting algorithm to validate.
 
         Returns:
-            List[List[str]]: A list of token lists.
+            bool: Whether the sorting algorithm is valid.
         """
-        input = self.docs
-        # If all items are of the same data type
-        if all(isinstance(sub, type(input[0])) for sub in input[1:]):
-            # If all items are strings
-            if all(isinstance(sub, str) for sub in input[1:]):
-                return input
-            # If all items are objects
-            elif all(isinstance(sub, object) for sub in input[1:]):
-                return [[token.text for token in doc] for doc in input]
-            else:
-                raise Exception("Input contained an unrecognized data type.")
-        else:
-            raise Exception("All items in the list must be of the same data type.")
-
-
-# The code below is technically superseded by the DTM class,
-# but, it is a nice convenience method.
-
-
-def get_doc_term_counts(
-    docs: List[object],
-    limit: int = None,
-    start: Any = 0,
-    end: Any = None,
-    filters: List[Union[Dict[str, str], str]] = None,
-    regex: bool = False,
-    normalize: bool = False,
-    normalize_with_filters: bool = False,
-    as_df=False,
-) -> Union[List, pd.DataFrame]:
-    """Get a list of word counts for each token in the doc.
-
-    Args:
-        docs (List[object]): A list of spaCy docs.
-        limit (int): The maximum number of tokens to count.
-        start (int): The index of the first token to count.
-        end (int): The index of the last token to count after limit is applied.
-        filters (List[Union[Dict[str, str], str]]): A list of Doc attributes to ignore.
-        regex (bool): Whether to match the dictionary value using regex.
-        normalize (bool): Whether to return raw counts or relative frequencies.
-        normalize_with_filters (bool): Whether to normalize based on the number of tokens after filters are applied.
-        as_df (bool): Whether to return a pandas dataframe.
-
-    Returns:
-        Union[List, pd.DataFrame]: A list of word count tuples for
-        each token in the doc. Alternatively, a pandas dataframe.
-    """
-    tokens = []
-    bool_filters = []
-    dict_filters = {}
-    if filters:
-        for filter in filters:
-            if isinstance(filter, dict):
-                dict_filters[list(filter.keys())[0]] = list(filter.values())[0]
-            else:
-                bool_filters.append(filter)
-    tokens = [
-        token.text
-        for doc in docs
-        for token in doc
-        if _bool_filter(token, bool_filters)
-        and _dict_filter(token, dict_filters, regex=regex)
-    ]
-    term_counts = Counter(tokens).most_common(limit)[start:end]
-    columns = ["term", "count"]
-    if normalize_with_filters:
-        normalize = True
-        num_tokens = len(tokens)
-    else:
-        num_tokens = sum([len(doc) for doc in docs])
-    if normalize:
-        term_counts = [(x[0], x[1] / num_tokens) for x in term_counts]
-        columns[1] = "frequency"
-    if as_df:
-        return pd.DataFrame(term_counts, columns=columns)
-    else:
-        return term_counts
-
-
-def _bool_filter(token: object, filters: List[str]) -> bool:
-    """Filter a token based on a list of boolean filters.
-
-    Args:
-        token (object): A spaCy token.
-        filters (str): A list of boolean filters (the names of spaCy token attributes).
-
-    Returns:
-        bool: Whether the token passes the filters.
-    """
-    if filters and filters != []:
-        for filter in filters:
-            if getattr(token, filter):
-                return False
-            else:
-                return True
-    else:
-        return True
-
-
-def _dict_filter(token, filters: List[Dict[str, str]], regex: bool = False) -> bool:
-    """Filter a token based on a list of dictionary filters.
-
-    Args:
-        token (object): A spaCy token.
-        filters (List[Dict[str, str]]): A list of filter dictionaries with keys as spaCy token attributes.
-        regex (bool): Whether to match the dictionary value using regex.
-
-    Returns:
-        bool: Whether the token passes the filters.
-    """
-    if filters and filters != {}:
-        for filter, value in filters.items():
-            if (
-                regex
-                and re.search(re.compile(value), getattr(token, filter)) is not None
-            ):
-                return False
-            elif getattr(token, filter) == value:
-                return False
-            else:
-                return True
-    else:
+        if alg not in [e for e in ns]:
+            locales = ", ".join([f"ns.{e.name}" for e in ns])
+            err = (
+                f"Invalid sorting algorithm: {alg}.",
+                f"Valid algorithms for `alg` are: {locales}.",
+                "See https://natsort.readthedocs.io/en/stable/api.html#natsort.ns.",
+            )
+            raise LexosException(" ".join(err))
         return True
