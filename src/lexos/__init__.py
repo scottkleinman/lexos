abc91cb69d1cad0a509f3b5671f44d4d455aa887
scottkleinman <scott.kleinman@csun.edu>
2026-08-09 09:22:54 -0700

    Add more metadata

--- src/lexos/__init__.py
+++ src/lexos/__init__.py
@@ -1,5 +1,10 @@
 """__init__.py."""
 
+__title__ = "Lexos"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022- The Lexomics Project"
+__status__ = "pre-release"
 __version__ = "0.2.0-pre-release"
+__version_info__ = (0, 2, 0, "pre-release")
 __docs__ = "https://scottkleinman.github.io/lexos/"
 __repo__ = "https://github.com/scottkleinman/lexos"
