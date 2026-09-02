b816a58731c02c3c63e0e0e4f4806a6f997b9181
scottkleinman <scott.kleinman@csun.edu>
2026-09-02 14:41:30 -0700

    Normalise version

--- src/lexos/__init__.py
+++ src/lexos/__init__.py
@@ -15,8 +15,8 @@ __title__ = "Lexos"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2022- The Lexomics Project"
 __status__ = "beta"
-__version__ = "0.2.0-beta"
-__version_info__ = (0, 2, 0, "beta")
+__version__ = "0.2.0b0"
+__version_info__ = (0, 2, 0, "b0")
 __docs__ = "https://scottkleinman.github.io/lexos/"
 __repo__ = "https://github.com/scottkleinman/lexos"
 
